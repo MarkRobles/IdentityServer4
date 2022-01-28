9bc8ba688c84c4be04d323fa92ba70ff5bf78b97
Marcos Ivan Robles Hernandez <marcos.robles@dalton.com.mx>
2022-01-28 10:47:49 -0600

    # No funciono

--- src/IdentityServer/Config.cs
+++ src/IdentityServer/Config.cs
@@ -71,7 +71,26 @@ namespace IdentityServer
                  IdentityServerConstants.StandardScopes.Address
                 ,"api1"
             }
-        }
+        },
+        // JavaScript Client
+new Client
+{
+    ClientId = "js",
+    ClientName = "JavaScript Client",
+    AllowedGrantTypes = GrantTypes.Code,
+    RequireClientSecret = false,
+
+    RedirectUris =           { "https://localhost:44332/callback.html" },
+    PostLogoutRedirectUris = { "https://localhost:44332/index.html" },
+    AllowedCorsOrigins =     { "https://localhost:44332" },
+
+    AllowedScopes =
+    {
+        IdentityServerConstants.StandardScopes.OpenId,
+        IdentityServerConstants.StandardScopes.Profile,
+        "api1"
+    }
+}
 
 
     };
